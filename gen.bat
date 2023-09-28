bce369782566b47f4f6602d11edb08f1d1cc854a
shan333chao <shan333chao@qq.com>
2023-09-28 10:11:39 +0800

    自动编译

--- gen.bat
+++ gen.bat
@@ -1,33 +1,39 @@
+del C:\DriverCodes\HideDriver\NickolasZhao\x64\Release\*.sys
+del C:\DriverCodes\HideDriver\NickolasZhao\x64\Release\encrypt.png
 
-"C:\ProgramFiles (x86)\VisualStudio\2019\Community\Common7\IDE\devenv.com"  "C:\DriverCodes\HideDriver\驱动隐藏\驱动隐藏.sln" /rebuild Release   /project  C:\DriverCodes\HideDriver\驱动隐藏\SSS_Drivers\SSS_Drivers.vcxproj  
+"C:\ProgramFiles (x86)\VisualStudio\2019\Community\Common7\IDE\devenv.com"  "C:\DriverCodes\HideDriver\NickolasZhao\YSN.sln" /rebuild Release   /project  C:\DriverCodes\HideDriver\NickolasZhao\SSS_Drivers\SSS_Drivers.vcxproj  
 
-pause
 
-cd C:\DriverCodes\HideDriver\驱动隐藏\x64\Release\
 
-pause
+cd C:\DriverCodes\HideDriver\NickolasZhao\x64\Release\
+
+
 
 AutoBuild.exe
 
-pause
 
-del C:\DriverCodes\HideDriver\驱动隐藏\x64\Release\SSS_Drivers.*
 
-pause
+del C:\DriverCodes\HideDriver\NickolasZhao\x64\Release\SSS_Drivers.*
 
-"C:\ProgramFiles (x86)\VisualStudio\2019\Community\Common7\IDE\devenv.com"  "C:\DriverCodes\HideDriver\驱动隐藏\驱动隐藏.sln" /build Release   /project  C:\DriverCodes\HideDriver\驱动隐藏\ProxyDrv\ProxyDrv.vcxproj
 
-pause
 
-copy "C:\DriverCodes\HideDriver\驱动隐藏\x64\Release\ProxyDrv\ProxyDrv.sys" "C:\DriverCodes\HideDriver\驱动隐藏\x64\Release\ProxyDrv.sys"
+"C:\ProgramFiles (x86)\VisualStudio\2019\Community\Common7\IDE\devenv.com"  "C:\DriverCodes\HideDriver\NickolasZhao\YSN.sln" /build Release   /project  C:\DriverCodes\HideDriver\NickolasZhao\ProxyDrv\ProxyDrv.vcxproj
 
-pause
 
-cd "C:\DriverCodes\HideDriver\驱动隐藏\x64\Release\"
+
+cd "C:\DriverCodes\HideDriver\NickolasZhao\x64\Release\"
 
 ClearPEdbg.exe
 
-pause
 
-del C:\DriverCodes\HideDriver\驱动隐藏\x64\Release\ProxyDrv.*
-del C:\DriverCodes\HideDriver\驱动隐藏\x64\Release\encrypt.png
+
+del C:\DriverCodes\HideDriver\NickolasZhao\x64\Release\ProxyDrv.*
+del C:\DriverCodes\HideDriver\NickolasZhao\x64\Release\encrypt.png
+
+C:\ProgramFiles\DSigntool\CSignTool.exe sign /r dandan /f C:\DriverCodes\HideDriver\NickolasZhao\x64\Release\ProxyDrv_nodbg.sys /ac /kp /s
+
+ren ProxyDrv_nodbg.sys ProxyDrv.sys
+
+cd "C:\DriverCodes\HideDriver\NickolasZhao\x64\Release\"
+
+ClearPEdbg.exe
