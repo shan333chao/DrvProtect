3fb8e9039e8f1d41648ab5ccf6484ae4fd30d0c7
shan333chao <shan333chao@qq.com>
2023-10-24 19:23:56 +0800

    update example

--- testMyblade/testMyblade.cpp
+++ testMyblade/testMyblade.cpp
@@ -3,15 +3,48 @@
 #include "FuncDef.h"
 
 
-HMODULE dllModule = LoadLibraryA("SSS_dll_dbg_x64.dll");
+HMODULE dllModule = LoadLibraryA("c:\\SSS_dll_dbg_x64.dll");
+WriteMemory write_memory = NULL;
+ReadMemory read_memory = NULL;
+InitReg reg = NULL;
+GetProcessIdByName getProcessIdByName = NULL;
+FakeProcess fuckProcess = NULL;
+ProtectWindow protectWindow = NULL;
+AntiSnapShotWindow antiSnapshot = NULL;
+AddProtectProcessR3  r3ProcessAccess = NULL;
+CreateMyThread createThread = NULL;
+AllocateMemmory allocMem = NULL;
+GetModuleExportAddr2 getModuleExport2 = NULL;
+QueryModule  queryModule = NULL;
+GetModuleExportAddr getModuleExport = NULL;
+InjectX64DLL inject = NULL;
+QueryVADModule queryVad = NULL;
+SearchPattern search = NULL;
+void InitFuncs() {
+	write_memory = (WriteMemory)GetProcAddress(dllModule, "_PhyWriteMemory");
+	read_memory = (ReadMemory)GetProcAddress(dllModule, "_PhyReadMemory");
+	reg = (InitReg)GetProcAddress(dllModule, "_InitReg");
+	getProcessIdByName = (GetProcessIdByName)GetProcAddress(dllModule, "_GetProcessIdByName");
+	fuckProcess = (FakeProcess)GetProcAddress(dllModule, "_ProtectProcess");
+	protectWindow = (ProtectWindow)GetProcAddress(dllModule, "_ProtectWindow");
+	antiSnapshot = (AntiSnapShotWindow)GetProcAddress(dllModule, "_AntiSnapShotWindow");
+	r3ProcessAccess = (AddProtectProcessR3)GetProcAddress(dllModule, "_ProtectProcessR3_Add");
+	createThread = (CreateMyThread)GetProcAddress(dllModule, "_CreateMyThread");
+	allocMem = (AllocateMemmory)GetProcAddress(dllModule, "_AllocateMem");
+	getModuleExport2 = (GetModuleExportAddr2)GetProcAddress(dllModule, "_GetModuleExportAddr2");
+	queryModule = (QueryModule)GetProcAddress(dllModule, "_QueryModule");
+	getModuleExport = (GetModuleExportAddr)GetProcAddress(dllModule, "_GetModuleExportAddr");
+	inject = (InjectX64DLL)GetProcAddress(dllModule, "_InjectX64DLL");
+	queryVad = (QueryVADModule)GetProcAddress(dllModule, "_QueryVADModule");
+	search = (SearchPattern)GetProcAddress(dllModule, "_SearchPattern");
 
- 
+}
 
 
 
 VOID ReadMemExample(ULONG pid) {
 
-	ReadMemory read_memory = (ReadMemory)GetProcAddress(dllModule, "_PhyReadMemory");
+
 
 	PVOID address = (PVOID)0x400000;
 	ULONG length = sizeof(ULONG);
@@ -31,7 +64,7 @@ VOID ReadMemExample(ULONG pid) {
 
 
 VOID WriteMemExample(ULONG pid) {
-	WriteMemory write_memory = (WriteMemory)GetProcAddress(dllModule, "_PhyWriteMemory");
+
 	ULONG length = sizeof(ULONG);
 	ULONG buffer = 0xccccccc;
 
@@ -48,7 +81,7 @@ VOID WriteMemExample(ULONG pid) {
 
 
 void RegExample() {
-	InitReg reg = (InitReg)GetProcAddress(dllModule, "_InitReg"); 
+
 
 	char code[] = "e120c826168d9828624d13355aa5b9ce8959ed0698ff16efc56ef05388356c1fe6cb16a48afe3a01ba021bb1bd9465b48f96";
 	//该方法仅需调用一次
@@ -61,14 +94,14 @@ void RegExample() {
 	{
 		printf("初始化失败 %08x \n", ret);
 		return;
-	} 
+	}
 }
 
 
- 
+
 
 ULONG GetProcessId(PCHAR name) {
-	GetProcessIdByName getProcessIdByName = (GetProcessIdByName)GetProcAddress(dllModule, "_GetProcessIdByName");
+
 	ULONG pid = 0;
 	ULONG ret = getProcessIdByName(name, &pid);
 	if (ret)
@@ -82,7 +115,7 @@ ULONG GetProcessId(PCHAR name) {
 
 
 
- 
+
 void FakeProcessExample() {
 
 	char name[] = "Wub_x64.exe";
@@ -96,7 +129,7 @@ void FakeProcessExample() {
 		printf("进程未找到");
 		return;
 	}
-	FakeProcess fuckProcess = (FakeProcess)GetProcAddress(dllModule, "_ProtectProcess");
+
 
 	ULONG ret = fuckProcess(protectPid, fakePid);
 	if (!ret)
@@ -110,16 +143,16 @@ void FakeProcessExample() {
 	}
 }
 
- 
+
 void ProtectWindowExample() {
 
-	ProtectWindow fuckProcess = (ProtectWindow)GetProcAddress(dllModule, "_ProtectWindow");
+
 	HWND hwnd = FindWindowA(NULL, "Windows Update Blocker v1.8");
 	if (!hwnd)
 	{
 		return;
 	}
-	ULONG ret = fuckProcess(HandleToULong(hwnd));
+	ULONG ret = protectWindow(HandleToULong(hwnd));
 	if (!ret)
 	{
 		printf("窗口保护成功 \n");
@@ -129,9 +162,9 @@ void ProtectWindowExample() {
 		printf("保护失败 错误码 %08x \n", ret);
 	}
 }
- 
+
 VOID SetAntiSnapShot() {
-	AntiSnapShotWindow antiSnapshot = (AntiSnapShotWindow)GetProcAddress(dllModule, "_AntiSnapShotWindow");
+
 	HWND hwnd = FindWindowA(NULL, "Detect It Easy v3.07 [Windows 10 Version 1909](x86_64)");
 	if (!hwnd)
 	{
@@ -150,11 +183,11 @@ VOID SetAntiSnapShot() {
 }
 
 
- 
+
 
 void SetProcessR3Access() {
 
-	AddProtectProcessR3  r3ProcessAccess = (AddProtectProcessR3)GetProcAddress(dllModule, "_ProtectProcessR3_Add");
+
 	ULONG pid = 7240;
 	ULONG ret = r3ProcessAccess(pid);
 	if (!ret)
@@ -168,9 +201,9 @@ void SetProcessR3Access() {
 	}
 }
 
- 
+
 void QueryModuleExample() {
-	QueryModule  queryModule = (QueryModule)GetProcAddress(dllModule, "_QueryModule");
+
 	ULONG pid = 8252;
 	char moduleName[] = "die.exe";
 	ULONG64 moduleBase = 0;
@@ -201,10 +234,10 @@ void QueryModuleExample() {
 	}
 }
 
- 
+
 VOID QueryVADModuleExample() {
 
-	QueryVADModule queryVad = (QueryVADModule)GetProcAddress(dllModule, "_QueryVADModule");
+
 	ULONG pid = 8252;
 	char moduleName[] = "die.exe";
 	ULONG64 moduleBase = 0;
@@ -221,13 +254,11 @@ VOID QueryVADModuleExample() {
 	}
 }
 
- 
 
-VOID AllocMemmoryExample() {
-	AllocateMemmory allocMem = (AllocateMemmory)GetProcAddress(dllModule, "_AllocateMem");
-	ULONG pid = 8252;
-	//10000个字节 长度
-	ULONG memSize = 10000;
+
+ULONG64 AllocMemmoryExample(ULONG pid, ULONG memSize) {
+
+
 	ULONG64 addr = 0;
 	ULONG ret = allocMem(pid, memSize, &addr);
 	if (!ret)
@@ -238,17 +269,57 @@ VOID AllocMemmoryExample() {
 	{
 		printf("\t    错误码 %08x \n", ret);
 	}
+	return addr;
 }
 
 
- 
+
 
 void CreateThreadExample() {
 
-	CreateMyThread createThread = (CreateMyThread)GetProcAddress(dllModule, "_CreateMyThread");
-	ULONG pid = 8252;
-	PVOID addr = (PVOID)0x123456;
-	ULONG ret = createThread(pid, &addr, NULL);
+
+	char moduleName[] = "die.exe";
+
+	ULONG pid = GetProcessId(moduleName);
+	if (!pid)
+	{
+		printf("\t进程未找到 \n");
+		return;
+	}
+	ULONG_PTR messageBox = (ULONG64)MessageBoxA;
+	//定义shellcode
+	UCHAR msgBoxShellcode[] = {
+		0x48, 0x83, 0xEC, 0x28,										//sub rsp,0x28
+		0x49, 0xC7, 0xC1, 0x01, 0x00, 0x00, 0x00,					//mov r9,0x1
+		0x49, 0xC7, 0xC0, 0x02, 0x00, 0x00, 0x00,					//mov r8,0x2
+		0x48, 0xC7, 0xC2, 0x03, 0x00, 0x00, 0x00,					//mov rcx,0x4
+		0x48, 0xC7, 0xC1, 0x04, 0x00, 0x00, 0x00,					//mov rcx,0x4
+		0x48, 0xB8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	//mov rax,0x12345678123
+		0xFF, 0xD0,													//call rax
+		0x48, 0x83, 0xC4, 0x28										//add rsp,0x28 
+	};
+	*(PULONG)(msgBoxShellcode + 34) = messageBox;
+
+
+	ULONG memSize = 0x1000;
+	//申请内存
+	ULONG64 addr = 0;
+	allocMem(pid, memSize,&addr);
+	if (addr==0)
+	{
+		printf("\t申请内存失败 \n");
+		return;
+	}
+
+
+
+	ULONG length = sizeof(msgBoxShellcode);
+	ULONG ret = write_memory(pid, (PVOID)addr, (PVOID)msgBoxShellcode, length);
+
+
+
+
+	ret = createThread(pid, &addr, NULL);
 	if (!ret)
 	{
 		printf("\t创建线程 成功 \n");
@@ -259,10 +330,10 @@ void CreateThreadExample() {
 	}
 }
 
- 
+
 VOID SearchPatternExample() {
 
-	SearchPattern search = (SearchPattern)GetProcAddress(dllModule, "_SearchPattern");
+
 	ULONG pid = 9828;
 	char moduleName[] = "die.exe";
 	char pattern[] = "\x48\x00\x00\x00\x33\xC9\xFF\x00\x00\x00\x00\x00\x48\x85\xC0\x74\x00\xB9\x00\x00\x00\x00\x66\x39\x08\x75\x00";
@@ -280,10 +351,10 @@ VOID SearchPatternExample() {
 }
 
 
- 
+
 VOID InjectX64DllExample() {
 
-	InjectX64DLL inject = (InjectX64DLL)GetProcAddress(dllModule, "_InjectX64DLL");
+
 	ULONG pid = 0;
 	char moduleName[] = "die.exe";
 	pid = GetProcessId(moduleName);
@@ -304,11 +375,11 @@ VOID InjectX64DllExample() {
 	}
 }
 
- 
+
 
 VOID GetModuleExportExample() {
 
-	GetModuleExportAddr getModuleExport = (GetModuleExportAddr)GetProcAddress(dllModule, "_GetModuleExportAddr");
+
 	ULONG pid = 0;
 	char processName[] = "ida64.exe";
 	pid = GetProcessId(processName);
@@ -331,11 +402,11 @@ VOID GetModuleExportExample() {
 	}
 }
 
- 
+
 
 void GetModuleExportExample2() {
 
-	GetModuleExportAddr2 getModuleExport = (GetModuleExportAddr2)GetProcAddress(dllModule, "_GetModuleExportAddr2");
+
 	ULONG pid = 0;
 	char processName[] = "ida64.exe";
 
@@ -346,7 +417,7 @@ void GetModuleExportExample2() {
 		return;
 	}
 
-	QueryModule  queryModule = (QueryModule)GetProcAddress(dllModule, "_QueryModule");
+
 	char moduleName[] = "ida64.dll";
 	ULONG64 moduleBase = 0;
 	ULONG moduleSize = 0;
@@ -359,7 +430,7 @@ void GetModuleExportExample2() {
 	char exportName[] = "MD5Final";
 	ULONG64 funcAddr = 0;
 
-	ret = getModuleExport(pid, moduleBase, exportName, &funcAddr);
+	ret = getModuleExport2(pid, moduleBase, exportName, &funcAddr);
 	if (!ret)
 	{
 		printf("\t 查询成功！ 0x%llx \n", funcAddr);
@@ -373,28 +444,30 @@ void GetModuleExportExample2() {
 
 int main()
 {
+	InitFuncs();
 	RegExample();
 
-	char name[] = "InstDrv.exe";
-	ULONG pid= GetProcessId(name);
-	if (!pid)
-	{
-		return 1;
-	}
-	printf("进程id %d \n", pid);
-	WriteMemExample(pid);
-	ReadMemExample(pid); 
-	FakeProcessExample();
-	ProtectWindowExample();
-	SetAntiSnapShot();
-	SetProcessR3Access();
-	QueryModuleExample();
-	QueryVADModuleExample();
-	AllocMemmoryExample();
-	SearchPatternExample();
-	InjectX64DllExample();
-	GetModuleExportExample();
-	GetModuleExportExample2();
+	//char name[] = "InstDrv.exe";
+	//ULONG pid = GetProcessId(name);
+	//if (!pid)
+	//{
+	//	return 1;
+	//}
+	//printf("进程id %d \n", pid);
+	//WriteMemExample(pid);
+	//ReadMemExample(pid); 
+	//FakeProcessExample();
+	//ProtectWindowExample();
+	//SetAntiSnapShot();
+	//SetProcessR3Access();
+	//QueryModuleExample();
+	//QueryVADModuleExample();
+	//AllocMemmoryExample();
+	//SearchPatternExample();
+	//InjectX64DllExample();
+	//GetModuleExportExample();
+	//GetModuleExportExample2();
+	CreateThreadExample();
 	getchar();
 
 
