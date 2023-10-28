a64a7c6a75b4c4dae07e28f1e52d04fdb97ba3e1
shan333chao <shan333chao@qq.com>
2023-10-28 17:49:02 +0800

    update config

---
+++ AutoBuild/Config.h
@@ -0,0 +1,5 @@
+#pragma once
+//驱动文件加密路径
+#define ENCRYPT_DRIVER "C:\\DriverCodes\\HideDriver\\NickolasZhao\\NewLoader\\MyDriver.h"
+//加载器加密路径
+#define ENCRYPT_LOADER "C:\\DriverCodes\\HideDriver\\NickolasZhao\\DiviceClient\\driver_shellcode.h"
